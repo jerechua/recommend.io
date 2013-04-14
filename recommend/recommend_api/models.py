af5527ddecb12452f668c3bd4e2e5a60387cc192
Jeremy <jere.chua@gmail.com>
2013-04-14 18:06:02 -0400

    changed models to add ratings

--- recommend/recommend_api/models.py
+++ recommend/recommend_api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth.models import User as django_auth_user
+
 
 class Anime(models.Model):
     anidb_id = models.IntegerField(primary_key=True, null=False, unique=True)
@@ -11,6 +13,7 @@ class Anime(models.Model):
     def __str__(self):
         return "%s - %s" % (self.anidb_id, self.title)
 
+
 class AnimeTitle(models.Model):
     anime = models.ForeignKey('Anime', null=False)
     locale = models.CharField(max_length=10)
@@ -19,3 +22,27 @@ class AnimeTitle(models.Model):
 
     def __str__(self):
         return "%s - %s (%s)" % (self.type, self.title, self.locale)
+
+
+class BaseModel(models.Model):
+    modify_time = models.DateTimeField(auto_now=True)
+    create_time = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class AnimeRating(BaseModel):
+    user = models.ForeignKey('User', null=False)
+    anime = models.ForeignKey('Anime', null=False)
+    rating = models.PositiveIntegerField()
+
+
+class UserProfile(django_auth_user):
+    picture = models.CharField(max_length=150)
+
+    def get_anime_rating(anime):
+        """
+        """
+
+        return AnimeRating.objects.filter(user=self, anime=anime).rating
