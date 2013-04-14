6af3d86b13196b8142b5fa24487f34477bef9671
Jeremy <jere.chua@gmail.com>
2013-04-13 22:50:32 -0400

    added str for models

--- recommend/recommend_api/models.py
+++ recommend/recommend_api/models.py
@@ -4,9 +4,18 @@ from django.db import models
 class Anime(models.Model):
     anidb_id = models.IntegerField(primary_key=True, null=False, unique=True)
 
+    @property
+    def title(self):
+        return self.animetitle_set.get(type='main').title
+
+    def __str__(self):
+        return "%s - %s" % (self.anidb_id, self.title)
 
 class AnimeTitle(models.Model):
     anime = models.ForeignKey('Anime', null=False)
     locale = models.CharField(max_length=10)
     title = models.CharField(max_length=150)
     type = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "%s - %s (%s)" % (self.anime, self.title, self.locale)
